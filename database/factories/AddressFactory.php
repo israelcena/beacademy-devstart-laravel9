37de77270ade6bf4358181174ba6cf3bbf8b1b4d
israelcena <israelsjm@gmail.com>
2022-06-30 14:44:29 -0300

    create address seeder

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'address' => $this->faker->address(),
+            'postal_code' => $this->faker->postcode(),
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country()
+        ];
+    }
+}
