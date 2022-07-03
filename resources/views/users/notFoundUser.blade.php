2bd7dcb84e5c3b3a56cfd9186ef67b4a3baf9d4f
Israel Cena <israelsjm@gmail.com>
2022-07-02 21:44:59 -0300

    create not found user page

---
+++ resources/views/users/notFoundUser.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Not Found</title>
+</head>
+<body>
+  <div>
+    <p>Usuário não encontrado</p>
+    <a href="{{ route('users.index') }}">Voltar para index</a>
+  </div>
+</body>
+</html>
