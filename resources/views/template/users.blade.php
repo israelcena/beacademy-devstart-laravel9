8e0782b1719e70cd1c864b8e9df8ac5e6f841835
Israel Cena <israelsjm@gmail.com>
2022-07-03 01:21:41 -0300

    include navbar

--- resources/views/template/users.blade.php
+++ resources/views/template/users.blade.php
@@ -8,8 +8,26 @@
    <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">  
 </head>
-<body class="container">
+<body>
+      <nav class="navbar navbar-expand-sm navbar-light bg-info">
+          <div class="container">
+              <a class="navbar-brand" href="#">Cadastro de Usuários</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                  aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarID">
+                  <ul class="navbar-nav">
+                      <li class="nav-item"><a class="nav-link {{ request()->routeIs('users.index') ? 'active' : '' }}" aria-current="page" href="/usuarios">Home</a></li>
+                      <li class="nav-item"><a class="nav-link {{ request()->routeIs('users.create') ? 'active' : '' }}" aria-current="page" href="/usuarios/criar">Criar Usuários</a></li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
+      <div class="container">
     @yield('body')
+  </div>
+    
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
