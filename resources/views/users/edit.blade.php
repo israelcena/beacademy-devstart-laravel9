94e33455a0013010d36057e73da42b0dc59a97b5
Israel Cena <israelsjm@gmail.com>
2022-07-03 23:29:45 -0300

    include edit user values

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,20 +1,20 @@
 @extends('template.users')
-@section('title', 'Criar Usuário')
+@section('title', "Editar Usuário {{ $user->name }}" )
 @section('body')
-
 <div class="container">  
-  <form class="row g-3 needs-validation" method="post" action="{{route('users.store')}}">
+  <form class="row g-3 needs-validation" method="POST" action="{{route('users.update', $user->id)}}">
+  @method('PUT')
   @csrf
   <div class="col-md-12">
     <label for="name" class="form-label">Nome</label>
-    <input type="text" class="form-control" id="name" name="name" value="Mark" required>
+    <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
   <div class="col-md-12">
     <label for="password" class="form-label">Senha</label>
-    <input type="password" class="form-control" id="password" name="password" value="Otto" required>
+    <input type="password" class="form-control" id="password" name="password">
     <div class="valid-feedback">
       Looks good!
     </div>
@@ -22,7 +22,7 @@
   <div class="col-md-12">
     <label for="email" class="form-label">E-mail</label>
     <div class="input-group">
-      <input type="email" class="form-control" id="email" name="email" aria-describedby="email" required>
+      <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" aria-describedby="email" required>
       <div class="invalid-feedback">
        Por Favor coloque um email válido.
       </div>
