98cac58a897a53eaa65d42a17a57c038012f06df
Israel Cena <israelsjm@gmail.com>
2022-07-03 23:45:25 -0300

    include edit button

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,6 +5,7 @@
     <h1>Central do Usuário</h1>
     <p>Olá {{ $user->name }}</p>
     <p>Seu email: {{ $user->email }}</p>
+    <button class="btn btn-warning"><a class="text-black" href="{{ route('users.edit', $user->id) }}">Editar</a></button>
     <button class="btn btn-primary"><a class="text-white" href="{{ route('users.index') }}">Voltar</a></button>
   </div>
   @endsection
