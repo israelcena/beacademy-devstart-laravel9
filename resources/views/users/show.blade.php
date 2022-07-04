b74a7e309fbf705ecda03dcbc5b13fd62b4897a1
Israel Cena <israelsjm@gmail.com>
2022-07-04 00:11:17 -0300

    include delete button

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,12 @@
     <h1>Central do Usuário</h1>
     <p>Olá {{ $user->name }}</p>
     <p>Seu email: {{ $user->email }}</p>
-    <button class="btn btn-warning"><a class="text-black" href="{{ route('users.edit', $user->id) }}">Editar</a></button>
-    <button class="btn btn-primary"><a class="text-white" href="{{ route('users.index') }}">Voltar</a></button>
+    <form action="{{ route('users.destroy', $user->id) }}" method="post">
+      @method('DELETE')
+      @csrf
+      <a class="btn btn-warning text-black" href="{{ route('users.edit', $user->id) }}">Editar</a>
+      <a class="btn btn-primary text-white" href="{{ route('users.index') }}">Voltar</a>
+      <button class="btn btn-danger text-white" type="submit">Excluir</button>
+    </form>
   </div>
   @endsection
