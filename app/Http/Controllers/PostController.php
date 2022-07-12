c0090dc2fcd9304acdf3019eab3d2a487220ef0f
israelcena <israelsjm@gmail.com>
2022-07-12 19:23:03 -0300

    create show all posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    // protected $user;
+    // protected $post;
+
+    // public function __construct(
+    //     User $user,
+    //     Post $post
+    // ) {
+    //     $this->user = $user;
+    //     $this->post = $post;
+    // }
+
+    public function __construct(
+        public User $user,
+        public Post $post
+    ) {
+    }
+
+    public function index($userId)
     {
-        return "Hello Posts!";
+        $user = $this->user->find($userId);
+        if (!$user) {
+            return view('posts.notfound');
+        }
+        $posts = $user->posts()->get();
+        return view('posts.index', compact('posts'));
     }
 }
