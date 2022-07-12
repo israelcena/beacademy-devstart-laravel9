9a7d65572c72feb8588e08e2dfe0b30fd40c0ec7
israelcena <israelsjm@gmail.com>
2022-07-12 00:07:03 -0300

    include img in view

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,10 +1,11 @@
 @extends('template.users')
 @section('title', "Usuário {$user->name}")
 @section('body')
-  <div>
-    <h1>Central do Usuário</h1>
-    <p>Olá {{ $user->name }}</p>
-    <p>Seu email: {{ $user->email }}</p>
+  <div class="container justify-center">
+    <h1 class="mt-4">Central do Usuário</h1>
+    <p>Olá {{ $user->name }},</p>
+    <div><img class="rounded-circle mb-4" width="100px" src="{{ asset("/storage/$user->image") }}" alt="{{ $user->name }}"/></div>
+    <p>Email Cadastrado: {{ $user->email }}</p>
     <form action="{{ route('users.destroy', $user->id) }}" method="post">
       @method('DELETE')
       @csrf
