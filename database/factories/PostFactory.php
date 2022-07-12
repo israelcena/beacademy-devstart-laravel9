62d21c96b874f7d0a1d3daac14388b6e7149e400
israelcena <israelsjm@gmail.com>
2022-07-12 19:23:14 -0300

    create post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'title' => $this->faker->sentence(4),
+            'post' => $this->faker->text(800),
+            'active' => true
+        ];
+    }
+}
