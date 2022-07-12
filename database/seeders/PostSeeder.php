9f676f3284c106bef5adea395b35b833903d92d2
israelcena <israelsjm@gmail.com>
2022-07-12 19:23:32 -0300

    create posts seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,19 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            AddressSeeder::class,
-            PostSeeder::class
-        ]);
+        \App\Models\Post::factory(330)->create();
     }
 }
