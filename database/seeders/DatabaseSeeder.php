da21b46e5de58236c113089750557a08fc359586
israelcena <israelsjm@gmail.com>
2022-07-12 21:20:32 -0300

    include Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             AddressSeeder::class,
-            PostSeeder::class
+            PostSeeder::class,
+            TeamSeeder::class
         ]);
     }
 }
