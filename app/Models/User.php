e00a2e531ce60a4c403e5622720f99e8f69b6e22
israelcena <israelsjm@gmail.com>
2022-07-12 21:37:26 -0300

    include relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function teams()
+    {
+        return $this->belongsToMany(Team::class);
+    }
 }
