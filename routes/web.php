39902b4d2ce206184ccb746436fdeee1fa080b27
israelcena <israelsjm@gmail.com>
2022-07-16 10:18:48 -0300

    change include class call

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\TeamController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ViaCepController;
-use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\{
+    UserController,
+    PostController,
+    TeamController,
+    ViaCepController,
+    WeatherController,
+    HealthCheck
+};
 use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
