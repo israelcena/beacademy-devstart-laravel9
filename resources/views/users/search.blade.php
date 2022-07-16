8773927dcf0f48df12f2a2661b669a9b4a37b2f5
israelcena <israelsjm@gmail.com>
2022-07-16 16:27:33 -0300

    create search view

---
+++ resources/views/users/search.blade.php
@@ -0,0 +1,51 @@
+@extends('template.users')
+@section('title', 'Listagem dos Usuários')
+@section('body')
+    <h1 class="mt-4">Listagem de Usuários</h1>
+    <table class="table table-striped mt-4">
+      <thead class="table-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Foto</th>
+      <th scope="col">Nome</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Qtd de Posts</th>
+      <th scope="col">Criado em</th>
+      <th scope="col">Ações</th>
+    </tr>
+  </thead>
+      <tbody>
+        @foreach($users as $user)
+        <tr>
+          <th scope="row">{{ $user->id }}</td>
+          <td>
+            @if($user->image)
+            <img width="50px" class="rounded-circle" src="{{ asset("/storage/$user->image") }}" alt="{{ $user->name }}" />
+            @else
+            <img width="50px" class="rounded-circle" src="{{ asset("/storage/profile/usuario.jpg") }}" alt="{{ $user->name }}" />
+            @endif
+          </td>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+          <td>{{ $user->posts->count() }}</td>
+          <td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+          <td>
+            <div class="btn-group">
+              <a href="{{ route('users.show', $user->id) }}" class="btn btn-primary btn-sm">Visualisar</a>
+              <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning btn-sm">Editar</a>
+              <form action="{{ route('users.destroy', $user->id) }}" method="post">
+              @method('DELETE')
+              @csrf
+              <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
+              </form>
+            </div>
+          </td>
+        </tr>
+      @endforeach
+       </tbody>    
+    </table>
+    <div class="justify-content-center pagination">
+      {{ $users->links('pagination::bootstrap-4') }}
+    </div>
+    
+@endsection
