ddfe0e845fa01fcf9f445da8e4ae6ee8f7bed99a
Israel Cena <israelsjm@gmail.com>
2022-07-17 18:11:54 -0300

    include comments for insert via Illuminate Facades

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -19,5 +20,6 @@ class UserSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // DB::table('User')->insert([]);
     }
 }
