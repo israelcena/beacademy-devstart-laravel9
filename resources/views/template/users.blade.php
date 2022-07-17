40d8c902ec09776529b20f562536cfcc61529555
Israel Cena <israelsjm@gmail.com>
2022-07-17 14:16:36 -0300

    remove div container

--- resources/views/template/users.blade.php
+++ resources/views/template/users.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +8,6 @@
   <!-- Bootstrap CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-
 <body>
   @section('navbar')
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
@@ -34,12 +32,8 @@
   </nav>
   @endsection
 
-  <div class="container">
-    @yield('body')
-  </div>
-
+  @yield('body')
 </body>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-
 </html>
