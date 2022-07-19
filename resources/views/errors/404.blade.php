b2a85fed71739bf301f2e6bf98d8b4482d1ea233
israelcena <israelsjm@gmail.com>
2022-07-19 19:01:58 -0300

    create 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('template.users')
+@section('title', 'Página não encontrada!')
+@section('body')
+<div class="container text-center my-4">
+<h1 class="mt-4">404 - Página não encontrada!</h1>
+<a href="/" class="btn btn-lg btn-secondary mt-4">Voltar para Home</a>
+</div>
+@endsection
